e43ca19eae8a1cf6f2541cae021a98876f8f78f4
wimvanhenden-tool <wim.vanhenden@toolofna.com>
2023-09-11 10:58:30 +0200

    addes styles and seed

--- app.py
+++ app.py
@@ -38,6 +38,56 @@ app = App(
     volumes=[Volume(name="models", path="./models")],
 )
 
+def set_style(prompt, style):
+
+    final_prompt =f""
+    negative_prompt = f""
+
+    if style == "3D Model":
+        final_prompt= f"professional 3d model {prompt} . octane render, highly detailed, volumetric, dramatic lighting"
+        negative_prompt = f"ugly, deformed, noisy, low poly, blurry, painting"
+    elif style == "Analog Film":
+        final_prompt= f"analog film photo {prompt} . faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage"
+        negative_prompt = f"painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured"
+    elif style == "Anime":
+        final_prompt= f"anime artwork {prompt} . anime style, key visual, vibrant, studio anime,  highly detailed"
+        negative_prompt = f"photo, deformed, black and white, realism, disfigured, low contrast"
+    elif style == "Cinematic":
+        final_prompt= f"cinematic film still {prompt} . shallow depth of field, vignette, highly detailed, high budget, bokeh, cinemascope, moody, epic, gorgeous, film grain, grainy"
+        negative_prompt = f"anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured"
+    elif style == "Pencil Sketch":
+        final_prompt= f"pencil sketch {prompt}, graphic"
+        negative_prompt = f"photo, deformed, realism, disfigured, deformed, glitch, noisy, realistic"
+    elif style == "Comic Book":
+        final_prompt= f"comic {prompt} . graphic illustration, comic art, graphic novel art, vibrant, highly detailed"
+        negative_prompt = f"photograph, deformed, glitch, noisy, realistic, stock photo"
+    elif style == "Digital Art":
+        final_prompt= f"concept art {prompt} . digital artwork, illustrative, painterly, matte painting, highly detailed"
+        negative_prompt = f"photo, photorealistic, realism, ugly"
+    elif style == "Line Art":
+        final_prompt= f"cline art drawing {prompt} . professional, sleek, modern, minimalist, graphic, line art, vector graphics"
+        negative_prompt = f"anime, photorealistic, 35mm film, deformed, glitch, blurry, noisy, off-center, deformed, cross-eyed, closed eyes, bad anatomy, ugly, disfigured, mutated, realism, realistic, impressionism, expressionism, oil, acrylic"
+    elif style == "Lowpoly":
+        final_prompt= f"low-poly style {prompt} . low-poly game art, polygon mesh, jagged, blocky, wireframe edges, centered composition"
+        negative_prompt = f"noisy, sloppy, messy, grainy, highly detailed, ultra textured, photo"
+    elif style == "Photographic":
+        final_prompt= f"cinematic photo {prompt} . 35mm photograph, film, bokeh, professional, 4k, highly detailed"
+        negative_prompt = f"drawing, painting, crayon, sketch, graphite, impressionist, noisy, blurry, soft, deformed, ugly"
+    elif style == "Abstract":
+        final_prompt= f"Abstract style {prompt} . Non-representational, colors and shapes, expression of feelings, imaginative, highly detailed"
+        negative_prompt = f"realistic, photographic, figurative, concrete"
+    elif style == "Watercolor":
+        final_prompt= f"Watercolor painting {prompt} . Vibrant, beautiful, painterly, detailed, textural, artistic"
+        negative_prompt = f"anime, photorealistic, 35mm film, deformed, glitch, low contrast, noisy"
+    elif style == "Monochrome":
+        final_prompt= f"Monochrome {prompt} . Black and white, contrast, tone, texture, detailed"
+        negative_prompt = f"colorful, vibrant, noisy, blurry, deformed"
+    else:
+        final_prompt =prompt
+
+
+    return final_prompt,negative_prompt
+
 def load_models():
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -59,11 +109,18 @@ def doSDXL(**inputs):
     pipe, refiner = inputs["context"]
 
     prompt = inputs["prompt"]
+    style = inputs["style"]
+    seed = inputs["seed"]
 
-    seed = random.randint(0, MAX_SEED)
-    generator = torch.Generator().manual_seed(seed)
+    if(seed == 0):
+        seed = random.randint(0, MAX_SEED)
     
-    int_image = pipe(prompt, prompt_2="", negative_prompt="", negative_prompt_2="", num_inference_steps=50, height=1024, width=1024, guidance_scale=10, num_images_per_prompt=1, generator=generator, output_type="latent").images
+    generator = torch.Generator().manual_seed(seed)
+
+    fprompt, fnegprompt = set_style(prompt,style)
+
+
+    int_image = pipe(fprompt, prompt_2="", negative_prompt=fnegprompt, negative_prompt_2="", num_inference_steps=50, height=1024, width=1024, guidance_scale=10, num_images_per_prompt=1, generator=generator, output_type="latent").images
  
     image = refiner(prompt=prompt, prompt_2="", negative_prompt="", negative_prompt_2="", image=int_image).images[0]   
     
@@ -71,7 +128,10 @@ def doSDXL(**inputs):
     image.save(buffered, format='JPEG',quality=80)
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-    return {"pred": img_str}
+    #print(fprompt)
+    #print(fnegprompt)
+
+    return {"image": img_str}
 
 
 if __name__ == "__main__":
